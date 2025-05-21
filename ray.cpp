9acf003332cce06ea401fd97ae1b311cb9d8a9e1
Jon Olsson <jon.olsson@gmail.com>
2025-05-21 16:23:47 +0200

    chapter 2

--- ray.cpp
+++ ray.cpp
@@ -1,5 +1,131 @@
 #include <iostream>
 
+struct vec3 {
+    vec3() = default;
+    vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+
+    inline float x() const { return e[0]; }
+    inline float y() const { return e[1]; }
+    inline float z() const { return e[2]; }
+
+    inline float r() const { return e[0]; }
+    inline float g() const { return e[1]; }
+    inline float b() const { return e[2]; }
+
+    inline const vec3&  operator+() const        { return *this; }
+    inline       vec3   operator-() const        { return vec3(-e[0], -e[1], -e[2]); }
+    inline       float  operator[](int i) const  { return e[i]; }
+    inline       float& operator[](int i)        { return e[i]; }
+
+    inline vec3& operator+=(const vec3& v2) {
+        e[0] += v2[0];
+        e[1] += v2[1];
+        e[2] += v2[2];
+        return *this;
+    }
+
+    inline vec3& operator-=(const vec3& v2) {
+        e[0] -= v2[0];
+        e[1] -= v2[1];
+        e[2] -= v2[2];
+        return *this;
+    }
+
+    inline vec3& operator*=(const vec3& v2) {
+        e[0] *= v2[0];
+        e[1] *= v2[1];
+        e[2] *= v2[2];
+        return *this;
+    }
+
+    inline vec3& operator/=(const vec3& v2) {
+        e[0] /= v2[0];
+        e[1] /= v2[1];
+        e[2] /= v2[2];
+        return *this;
+    }
+
+    inline vec3& operator*=(const float t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+
+        return *this;
+    }
+
+    inline vec3& operator/=(const float t) {
+        e[0] /= t;
+        e[1] /= t;
+        e[2] /= t;
+
+        return *this;
+    }
+
+    inline float length() const {
+        return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+    }
+
+    inline float squared_length() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    void make_unit_vector() {
+        float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1]+ e[2]*e[2]);
+        e[0] *= k; e[1] *= k; e[2] *= k;
+    }
+
+    float e[3];
+};
+
+inline std::istream& operator>>(std::istream& is, vec3& t) {
+    return is >> t.e[0] >> t.e[1] >> t.e[2];
+}
+
+inline std::ostream& operator<<(std::ostream& os, const vec3& t) {
+    return os << t.e[0] << t.e[1] << t.e[2];
+}
+
+inline vec3 operator+(const vec3& v1, const vec3 &v2) {
+    return vec3(v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]);
+}
+
+inline vec3 operator-(const vec3& v1, const vec3 &v2) {
+    return vec3(v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]);
+}
+
+inline vec3 operator*(const vec3& v1, const vec3 &v2) {
+    return vec3(v1[0] * v2[0], v1[1] * v2[1], v1[2] * v2[2]);
+}
+
+inline vec3 operator/(const vec3& v1, const vec3 &v2) {
+    return vec3(v1[0] / v2[0], v1[1] / v2[1], v1[2] / v2[2]);
+}
+
+inline vec3 operator/(vec3 v, float t) {
+    return vec3(t/v[0], t/v[1], t/v[2]);
+}
+
+inline vec3 operator*(vec3 v, float t) {
+    return vec3(t*v[0], t*v[1], t*v[2]);
+}
+
+inline float dot(const vec3& v1, const vec3& v2) {
+    return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
+}
+
+inline vec3 cross(const vec3& v1, const vec3& v2) {
+    return vec3(
+          v1[1]*v2[2] - v1[2]*v2[1],
+        -(v1[0]*v2[2] - v1[2]*v2[0]),
+          v1[0]*v2[1] - v1[1]*v2[0]
+    );
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
+
 int main() {
     int nx = 200;
     int ny = 100;
@@ -7,12 +133,10 @@ int main() {
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99*col.r());
+            int ig = int(255.99*col.g());
+            int ib = int(255.99*col.b());
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
